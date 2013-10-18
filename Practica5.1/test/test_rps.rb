2f97cda5f84db4c46f5f628e09b26cf925f722f1
Madelaine Martin <made_2503@hotmail.com>
2013-10-18 19:20:47 +0100

    Terminando Test Unitarios

--- Practica5.1/test/test_rps.rb
+++ Practica5.1/test/test_rps.rb
@@ -1,7 +1,46 @@
 require "test/unit"
 require "rack/test"
-require "./RockPaperScissors"
+require "./lib/RockPaperScissors"
 
 class RPSTest < Test::Unit::TestCase
 	include Rack::Test::Methods
+
+	def app
+		Rack::Builder.new do
+			run RockPaperScissors::RPS.new	
+		end
+	end
+
+	def test_index
+		get "/"
+		#puts last_response.inspect
+		assert last_response.ok?
+	end
+
+	def test_header
+		get "/"
+		last_response.header == 'Content-Length'
+	end
+
+
+	def test_status
+		get "/test-url", {}, {"HTTP_IF_NONE_MATCH" => '"15-xyz"'}
+		last_response.status == 200
+	end
+
+	def body
+		super.join
+	end
+
+	def assert_content(type)
+		assert_equal type, response.headers['Content-Type']
+	end
+
+	def empty?
+		[201,204,304,404].include? status
+	end
+
+	def assert_status(code)
+		assert_equal 200, response.status
+	end
 end
