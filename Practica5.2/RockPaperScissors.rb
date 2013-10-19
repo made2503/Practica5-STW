0898e873eb38d8846e3f45c77b8d20fd66256dfe
Madelaine Martin <made_2503@hotmail.com>
2013-10-19 14:51:15 +0100

    Cookies y Sesion

--- Practica5.2/RockPaperScissors.rb
+++ Practica5.2/RockPaperScissors.rb
@@ -6,7 +6,18 @@ builder = Rack:: Builder.new do
 	use Rack::ShowExceptions
 	use Rack::Lint
 	run RockPaperScissors::RPS.new
+	use Rack::Session::Cookie,
+		:key => 'rack.session',
+		:domain => 'rps.com',
+		:secret => 'some_secret',
+		:expire_after => 30
+
+	run RockPaperScissors::RPS.new
 end
 
-Rack::Handler::Thin.run builder
-
+use Rack::Server.start(
+	:app => builder,
+	:Port => 8080,
+	:server => 'thin',
+	:host => 'www.rps.com'
+	)
--- Practica5.2/lib/RockPaperScissors.rb
+++ Practica5.2/lib/RockPaperScissors.rb
@@ -3,7 +3,8 @@ require 'rack/response'
 require 'haml'
 require 'thin'
 require 'rack'
-p "Iniciando Servidor en: http://localhost:8080"
+p "Vaya a la página http://www.rps.com:8080"
+
 module RockPaperScissors
 	class RPS
 
@@ -14,7 +15,53 @@ module RockPaperScissors
 			@throws = ''
 		end
 
+		def haml(template, result)
+			template_file = File.open(template, 'r')
+			Haml::Engine.new(File.read(template_file)).render({}, result)
+		end
+
+		def set_env(env)
+			@env = env
+			@session = env['rack.session']
+		end
+
+		def won
+			return @session['won'].to_i if @session['won']
+			@session['won'] = 0
+		end
+
+		def won=(value)
+			@session['won'] = value
+		end
+
+		def lost
+			return @session['lost'].to_i if @session['lost']
+			@session['lost'] = 0
+		end
+
+		def lost=(value)
+			@session['lost'] = value
+		end
+
+		def tied
+			return @session['tied'].to_i if @session['tied']
+			@session['tied'] = 0
+		end
+
+		def tied=(value)
+			@session['tied'] = value
+		end
+		def play
+			return @session['play'].to_i if @session['play']
+			@session['play'] = 0
+		end
+
+		def play=(value)
+			@session['play'] = value
+		end
+
 		def call env
+			set_env(env)
 			req = Rack::Request.new(env)
 			player_throw = req.GET["choice"]
 			@throws = @defeat.keys
@@ -23,6 +70,7 @@ module RockPaperScissors
 				aux = "Choose one"
 			else
 				computer_throw = @throws.sample
+				self.play= self.play + 1
 			end
      		
 			anwser = 
@@ -34,16 +82,30 @@ module RockPaperScissors
 					"LOSE"
 				end
 
-			engine = Haml::Engine.new File.open("views/index.html.haml").read
-			res = Rack::Response.new
-			
-			res.write engine.render({},
-				:anwser => anwser,
-				:choose => @choose,
-				:throws => @throws,
-				:computer_throw => computer_throw,
-				:player_throw => player_throw,
-				:aux => aux)
+			if anwser == "WIN"
+				self.won= self.won + 1
+			elsif anwser == "LOSE"
+				self.lost= self.lost + 1
+			elsif anwser == "TIE"
+				self.tied= self.tied + 1
+			end
+
+			result =
+				{
+					:anwser => anwser,
+					:choose => @choose,
+					:throws => @throws,
+					:computer_throw => computer_throw,
+					:player_throw => player_throw,
+					:aux => aux,
+					:win => self.won,
+					:playit => self.play,
+					:lose => self.lost,
+					:tie => self.tied,
+				}
+
+			res = Rack::Response.new(haml("views/index.html.haml", result))
+			#res.write("some_key = #{@session['some_key']}\n")
 			res.finish
 		end #call
 	end #Clase
